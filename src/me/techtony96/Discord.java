094a0c25681807382cc6debc5d763e5ba88954a8
Tony Pappas <techtony96@gmail.com>
2016-07-08 20:38:10 -0400

    Change log level back to INFO

--- src/me/techtony96/Discord.java
+++ src/me/techtony96/Discord.java
@@ -3,7 +3,6 @@ package me.techtony96;
 import me.techtony96.modules.tempchannels.TempChannelModule;
 import me.techtony96.utils.Config;
 import me.techtony96.utils.Logger;
-import sx.blah.discord.Discord4J;
 import sx.blah.discord.modules.ModuleLoader;
 import sx.blah.discord.util.DiscordException;
 
@@ -11,8 +10,6 @@ public class Discord {
 
 	public static void main(String[] args) {
 		Logger.info("Discord.java starting up!");
-		
-		((Discord4J.Discord4JLogger) Discord4J.LOGGER).setLevel(Discord4J.Discord4JLogger.Level.TRACE);
 		
 		loadModules();
 
