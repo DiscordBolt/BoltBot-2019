f64d26b12e2911829c70e7624ae0e6478191fd01
Tony Pappas <techtony96@gmail.com>
2016-10-02 00:59:52 -0400

    Completed the most basic functioning parts of Game Tracker

--- src/main/java/Techtony96/Discord/modules/gametracker/GameTrackerModule.java
+++ src/main/java/Techtony96/Discord/modules/gametracker/GameTrackerModule.java
@@ -1,17 +1,19 @@
 package Techtony96.Discord.modules.gametracker;
 
-import Techtony96.Discord.modules.userstatus.UserStatusModule;
 import Techtony96.Discord.utils.Logger;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventSubscriber;
-import sx.blah.discord.handle.impl.events.PresenceUpdateEvent;
 import sx.blah.discord.handle.impl.events.ReadyEvent;
+import sx.blah.discord.handle.impl.events.StatusChangeEvent;
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.handle.obj.Status;
 import sx.blah.discord.modules.IModule;
 
+import java.util.HashMap;
+
 /**
- * Created by Techt on 9/29/2016.
+ * Created by Tony Pappas on 9/29/2016.
  */
 public class GameTrackerModule implements IModule {
 
@@ -20,8 +22,12 @@ public class GameTrackerModule implements IModule {
 	private String moduleVersion = "1.0";
 	private String moduleMinimumVersion = "2.5.0-SNAPSHOT";
 	private String author = "Techtony96";
-	protected static final String FILE_LOCATION = "/var/www/api/discord/user/";
+
 	protected static final String FILE_TYPE = ".txt";
+	protected static final String FILE_LOCATION = "/var/www/html/api/discord/gametracker/users";
+	protected static final String GAME_INDEX_LOCATION = "/var/www/html/api/discord/gametracker/games" + FILE_TYPE;
+
+	private HashMap<IUser, Long> currentUsers = new HashMap<>();
 
 	@Override
 	public void disable() {
@@ -30,7 +36,7 @@ public class GameTrackerModule implements IModule {
 
 	@Override
 	public boolean enable(IDiscordClient client) {
-		UserStatusModule.client = client;
+		GameTrackerModule.client = client;
 		return true;
 	}
 
@@ -56,12 +62,22 @@ public class GameTrackerModule implements IModule {
 
 	@EventSubscriber
 	public void onReady(ReadyEvent e) {
+		updateAllUsers();
 		Logger.info("[Discord.java] " + getName() + " version " + getVersion() + " is READY.");
 	}
 
 	@EventSubscriber
-	public void onUserGameUpdate(PresenceUpdateEvent e) {
-		// TODO Called when a user starts/stops playing a game
+	public void onUserGameUpdate(StatusChangeEvent e) {
+		if (e.getNewStatus().getType() == Status.StatusType.NONE) {
+			if (currentUsers.get(e.getUser()) == null) {
+				// User is not stored in the list, we somehow missed him so we will ignore that he just stopped playing a game.
+				Logger.warning(e.getUser().getName() + " was not in currentUsers to store his game data.");
+				return;
+			}
+			FileIO.addGameTime(e.getUser(), e.getOldStatus().getStatusMessage(), currentUsers.get(e.getUser()), System.currentTimeMillis());
+		} else if (e.getNewStatus().getType() == Status.StatusType.GAME) {
+			currentUsers.put(e.getUser(), System.currentTimeMillis());
+		}
 	}
 
 	/**
@@ -70,9 +86,9 @@ public class GameTrackerModule implements IModule {
 	private void updateAllUsers() {
 		for (IGuild guild : client.getGuilds()) {
 			for (IUser user : guild.getUsers()) {
-				if (user.isBot())
+				if (user.isBot() || user.getStatus().getType() == Status.StatusType.NONE || user.getStatus().getType() == Status.StatusType.STREAM)
 					continue;
-				// TODO Get all players playing a game (Used when a bot starts up)
+				currentUsers.put(user, System.currentTimeMillis());
 			}
 		}
 	}
