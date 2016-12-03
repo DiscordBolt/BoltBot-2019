ac20d85c536981e14e912b159f9d795c351240ac
Anthony Pappas <ajpappas@ncsu.edu>
2016-12-02 19:40:58 -0500

    Create !Disconnect command

--- src/main/java/Techtony96/Discord/utils/ExceptionMessage.java
+++ src/main/java/Techtony96/Discord/utils/ExceptionMessage.java
@@ -3,4 +3,5 @@ package Techtony96.Discord.utils;
 public interface ExceptionMessage {
 
 	String API_LIMIT = "Sending Discord too many requests. Rate limit hit.";
+	String PERMISSION_DENIED = "You do not have permission for this command!";
 }
