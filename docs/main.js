505e3501fb0efe4e908d1829e55bdcefdecd4f99
jstrahler <jason.strahler@gmail.com>
2017-02-12 17:55:21 -0500

    Main js file

---
+++ docs/main.js
@@ -0,0 +1,8 @@
+function jsonCallback(json) {
+    console.log(json);
+}
+
+$.ajax({
+    url: "http://52.207.210.73/api/discord/gametracker/users/110926688455495680.json",
+    dataType: "jsonp"
+});
