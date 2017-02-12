91002879403f672e8a552d6c3c70a9e77e648879
jstrahler <jason.strahler@gmail.com>
2017-02-12 18:43:35 -0500

    another attempt

--- docs/main.js
+++ docs/main.js
@@ -1,8 +1,9 @@
-function jsonCallback(json) {
-    console.log(json);
+function foo(data)
+{
+    // do stuff with JSON
 }
 
-$.ajax({
-    url: "http://52.207.210.73/api/discord/gametracker/users/110926688455495680.json",
-    dataType: "json"
-});
+var script = document.createElement('script');
+script.src = 'http://52.207.210.73/api/discord/gametracker/users/110926688455495680.json'
+
+document.getElementsByTagName('head')[0].appendChild(script);
