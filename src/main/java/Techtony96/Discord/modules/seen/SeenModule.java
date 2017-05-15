14b797b6af797db801c6a561c2b366a12dd23e44
Tony Pappas <techtony96@gmail.com>
2017-05-15 17:49:38 -0400

    Move !Seen module data storage to local json files - Resolves #39

--- src/main/java/Techtony96/Discord/modules/seen/SeenModule.java
+++ src/main/java/Techtony96/Discord/modules/seen/SeenModule.java
@@ -3,32 +3,28 @@ package Techtony96.Discord.modules.seen;
 import Techtony96.Discord.api.CustomModule;
 import Techtony96.Discord.api.commands.BotCommand;
 import Techtony96.Discord.api.commands.CommandContext;
-import Techtony96.Discord.api.mysql.MySQL;
-import Techtony96.Discord.utils.Logger;
 import Techtony96.Discord.utils.UserUtil;
 import org.ocpsoft.prettytime.PrettyTime;
 import sx.blah.discord.api.events.EventSubscriber;
 import sx.blah.discord.handle.impl.events.guild.GuildCreateEvent;
 import sx.blah.discord.handle.impl.events.guild.member.UserJoinEvent;
 import sx.blah.discord.handle.impl.events.user.PresenceUpdateEvent;
-import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
-import sx.blah.discord.handle.obj.StatusType;
 import sx.blah.discord.modules.IModule;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.*;
+import java.util.HashMap;
 
 /**
  * Created by Tony on 4/3/2017.
  */
 public class SeenModule extends CustomModule implements IModule {
 
+    private static HashMap<Long, UserStatus> statuses;
+
     public SeenModule() {
-        super("Seen Module", "1.0");
+        super("Seen Module", "1.1");
+        statuses = StatusFileIO.loadStatuses();
     }
 
     @BotCommand(command = "seen", module = "Seen Module", description = "See when the user was last online.", usage = "Seen @User", minArgs = 2, maxArgs = 100)
@@ -42,14 +38,13 @@ public class SeenModule extends CustomModule implements IModule {
             return;
         }
 
-        StatusType status = searchUser.getPresence().getStatus();
-        Timestamp lastUpate = getLastChange(searchUser);
-        if (lastUpate == null) {
+        UserStatus status = statuses.get(searchUser.getLongID());
+        if (status == null) {
             cc.replyWith("Sorry, I could not find \"" + name + "\".");
             return;
         }
 
-        cc.replyWith(searchUser.getName() + " has been " + status.name().toLowerCase().replace("dnd", "do not disturb") + " since " + format(lastUpate) + '.');
+        cc.replyWith(searchUser.getName() + " has been " + status.getStatus().name().replace("dnd", "do not disturb") + " since " + format(status.getLastUpdate()) + '.');
     }
 
     // This has been taken to GuildCreateEvent
@@ -60,15 +55,14 @@ public class SeenModule extends CustomModule implements IModule {
 
     @EventSubscriber
     public void onStatusChange(PresenceUpdateEvent e) {
-        if (e.getNewPresence().getStatus() != e.getOldPresence().getStatus()) {
-            // Update database
+        if (!statuses.containsKey(e.getUser().getLongID()) || e.getNewPresence().getStatus() != statuses.get(e.getUser().getLongID()).getStatus()) {
             updateUser(e.getUser());
         }
     }
 
     @EventSubscriber
     public void onJoinGuild(GuildCreateEvent e) {
-        updateAllUsers(Arrays.asList(e.getGuild()));
+        e.getGuild().getUsers().forEach(u -> updateUser(u));
     }
 
     @EventSubscriber
@@ -76,60 +70,22 @@ public class SeenModule extends CustomModule implements IModule {
         updateUser(e.getUser());
     }
 
-    private int updateUser(IUser user) {
-        try {
-            PreparedStatement ps = MySQL.getConnection().prepareStatement("REPLACE INTO `seen` (`user`, `lastUpdate`) VALUES (?,NOW());");
-            ps.setLong(1, user.getLongID());
-            return ps.executeUpdate();
-        } catch (SQLException e) {
-            Logger.error("Unable to update " + user.getName() + "'s presense in the database.");
-            Logger.debug(e);
-            return -1;
-        }
-    }
-
-    private int[] updateAllUsers(List<IGuild> guilds) {
-        Set<IUser> users = new HashSet<>();
-        guilds.forEach(g -> users.addAll(g.getUsers()));
-        Iterator<IUser> it = users.iterator();
-        try {
-            PreparedStatement ps = MySQL.getConnection().prepareStatement("REPLACE INTO `seen` (`user`, `lastUpdate`) VALUES (?,NOW());");
-            while (it.hasNext()) {
-                ps.setLong(1, it.next().getLongID());
-                ps.addBatch();
-                it.remove();
-            }
-            return ps.executeBatch();
-        } catch (SQLException e) {
-            Logger.error("Unable to update all users presense in the database.");
-            Logger.debug(e);
-            return new int[]{-1};
-        }
-    }
-
-    private void updateUsers() {
-        for (IGuild guild : client.getGuilds()) {
-            for (IUser user : guild.getUsers())
-                updateUser(user);
-        }
-    }
-
     private static String format(Timestamp time) {
         PrettyTime p = new PrettyTime();
         return p.format(time);
     }
 
-    private static Timestamp getLastChange(IUser user) {
-        try {
-            PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT `lastUpdate` FROM `seen` WHERE `user` = ?;");
-            ps.setLong(1, user.getLongID());
-            ResultSet rs = ps.executeQuery();
-            if (rs.next())
-                return rs.getTimestamp(1);
-        } catch (SQLException e) {
-            Logger.error("Unable to fetch " + user.getName() + "'s last presense change from the database.");
-            Logger.debug(e);
+    private void updateUser(IUser user) {
+        if (statuses.containsKey(user.getLongID())) {
+            UserStatus status = statuses.get(user.getLongID());
+            if (status.getStatus() != user.getPresence().getStatus()) {
+                status.updateStatus(user.getPresence().getStatus());
+                StatusFileIO.saveStatus(status);
+            }
+        } else {
+            UserStatus status = new UserStatus(user);
+            statuses.put(user.getLongID(), status);
+            StatusFileIO.saveStatus(status);
         }
-        return null;
     }
 }
