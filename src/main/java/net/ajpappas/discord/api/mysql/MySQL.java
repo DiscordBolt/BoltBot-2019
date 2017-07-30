d14d57acf770f8680adc6f79e05cb5d7727f3d13
Tony Pappas <techtony96@gmail.com>
2017-07-30 13:54:47 -0400

    Create MySQL properties files if it doesn't exist

--- src/main/java/net/ajpappas/discord/api/mysql/MySQL.java
+++ src/main/java/net/ajpappas/discord/api/mysql/MySQL.java
@@ -3,9 +3,12 @@ package net.ajpappas.discord.api.mysql;
 import com.mysql.cj.jdbc.MysqlDataSource;
 import net.ajpappas.discord.utils.Logger;
 
-import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 /**
@@ -14,11 +17,36 @@ import java.util.Properties;
 public class MySQL {
 
     private static MysqlDataSource dataSource = null;
+    private static Path propertiesPath = Paths.get(System.getProperty("user.dir"), "database.properties");
 
     private static void init() {
+        OutputStream output = null;
+        try {
+            if (!propertiesPath.toFile().exists()){
+                Properties prop = new Properties();
+                output = new FileOutputStream(propertiesPath.toFile());
+                prop.setProperty("USERNAME", "root");
+                prop.setProperty("PASSWORD", "password");
+                prop.setProperty("HOST", "localhost");
+                prop.setProperty("DATABASE", "Discord.java");
+                prop.store(output, "Discord.java MySQL Configuration");
+            }
+        } catch (IOException e){
+            Logger.error(e.getMessage());
+            Logger.debug(e);
+        } finally {
+            if (output != null){
+                try {
+                    output.close();
+                } catch (IOException e){
+                    Logger.error("Could not close MySQL Properties file.");
+                    Logger.debug(e);
+                }
+            }
+        }
         try {
             Properties props = new Properties();
-            props.load(new FileInputStream(System.getProperty("user.dir") + File.separator + "database.properties"));
+            props.load(new FileInputStream(propertiesPath.toFile()));
             dataSource = new MysqlDataSource();
             dataSource.setUser(props.getProperty("USERNAME"));
             dataSource.setPassword(props.getProperty("PASSWORD"));
