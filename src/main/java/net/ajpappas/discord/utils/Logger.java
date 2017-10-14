bcb0674920e9d4d712dc309c5edfcdbbbb2cfd08
Tony Pappas <techtony96@gmail.com>
2017-10-14 11:48:53 -0400

    Fix log formatting and saving to file - Fixes #84

--- src/main/java/net/ajpappas/discord/utils/Logger.java
+++ src/main/java/net/ajpappas/discord/utils/Logger.java
@@ -4,11 +4,6 @@ import sx.blah.discord.Discord4J;
 
 public class Logger {
 
-    private static final String COLOR_RESET = "\u001B[0m";
-    private static final String COLOR_INFO = "\u001B[36m";
-    private static final String COLOR_WARNING = "\u001B[33m";
-    private static final String COLOR_ERROR = "\u001B[31m";
-
     public static void debug(Exception e) {
         Discord4J.LOGGER.debug(e.getMessage(), e);
     }
@@ -18,14 +13,14 @@ public class Logger {
     }
 
     public static void info(String s) {
-        Discord4J.LOGGER.info(COLOR_INFO + s + COLOR_RESET);
+        Discord4J.LOGGER.info(s);
     }
 
     public static void warning(String s) {
-        Discord4J.LOGGER.warn(COLOR_WARNING + s + COLOR_RESET);
+        Discord4J.LOGGER.warn(s);
     }
 
     public static void error(String s) {
-        Discord4J.LOGGER.error(COLOR_ERROR + s + COLOR_RESET);
+        Discord4J.LOGGER.error(s);
     }
 }
