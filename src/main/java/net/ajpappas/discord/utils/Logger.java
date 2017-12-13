5cc3832f3a6383223cfe4097bd3256a60e23409c
Tony Pappas <techtony96@gmail.com>
2017-12-12 20:34:02 -0500

    Create our own slf4j logger instance

--- src/main/java/net/ajpappas/discord/utils/Logger.java
+++ src/main/java/net/ajpappas/discord/utils/Logger.java
@@ -1,30 +1,33 @@
 package net.ajpappas.discord.utils;
 
-import sx.blah.discord.Discord4J;
+import net.ajpappas.discord.Discord;
+import org.slf4j.LoggerFactory;
 
 public class Logger {
 
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(Discord.class);
+
     public static void trace(String s) {
-        Discord4J.LOGGER.trace(s);
+        LOGGER.trace(s);
     }
 
     public static void debug(Throwable e) {
-        Discord4J.LOGGER.debug(e.getMessage(), e);
+        LOGGER.debug(e.getMessage(), e);
     }
 
     public static void debug(String s) {
-        Discord4J.LOGGER.debug(s);
+        LOGGER.debug(s);
     }
 
     public static void info(String s) {
-        Discord4J.LOGGER.info(s);
+        LOGGER.info(s);
     }
 
     public static void warning(String s) {
-        Discord4J.LOGGER.warn(s);
+        LOGGER.warn(s);
     }
 
     public static void error(String s) {
-        Discord4J.LOGGER.error(s);
+        LOGGER.error(s);
     }
 }
