222907e12f80636d55a4529a5183f4c4c2eb3b7e
Tony Pappas <ajpappas@ncsu.edu>
2018-05-28 18:06:36 +0200

    readd deleted test

--- src/test/java/com/discordbolt/boltbot/web/controllers/IndexControllerTest.java
+++ src/test/java/com/discordbolt/boltbot/web/controllers/IndexControllerTest.java
@@ -19,12 +19,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class IndexControllerTest {
 
-    @Autowired private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void testIndex() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-           .andExpect(status().isOk())
-           .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().string(equalTo("Greetings from Spring Boot!")));
     }
 }
