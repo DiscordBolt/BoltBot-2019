a0b404f468157157f9994c781c89aa681d328515
Tony Pappas <ajpappas@ncsu.edu>
2018-05-28 18:08:47 +0200

    Add logging and test module

--- src/main/java/com/discordbolt/boltbot/BoltBot.java
+++ src/main/java/com/discordbolt/boltbot/BoltBot.java
@@ -1,12 +1,17 @@
 package com.discordbolt.boltbot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoltBot {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BoltBot.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting Spring application.");
         SpringApplication.run(BoltBot.class, args);
     }
 }
--- src/main/java/com/discordbolt/boltbot/discord/modules/HelloWorldModule.java
+++ src/main/java/com/discordbolt/boltbot/discord/modules/HelloWorldModule.java
@@ -1,16 +1,21 @@
 package com.discordbolt.boltbot.discord.modules;
 
+import com.discordbolt.boltbot.data.repositories.GuildRepository;
 import com.discordbolt.boltbot.discord.EventListener;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldModule extends EventListener<MessageCreateEvent> {
 
+    @Autowired
+    private GuildRepository guildRepository;
+
     @Override
     public void accept(MessageCreateEvent event) {
-        System.out.println("Received message!");
-        if (event.getMessage().getContent().isPresent() && event.getMessage().getContent().get().toLowerCase().equals("!hello"))
+        if (event.getMessage().getContent().isPresent() && event.getMessage().getContent().get().toLowerCase().equals("!hello")) {
             event.getMessage().getChannel().flatMap(channel -> channel.createMessage(spec -> spec.setContent("Hello World!"))).subscribe();
+        }
     }
 }
