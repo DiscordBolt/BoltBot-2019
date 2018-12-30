77f591e0f1a802c2191d68f66ba5ac331975edeb
Tony Pappas <techtony96@gmail.com>
2018-12-29 22:18:58 -0500

    Implement !Disconnect Command - Closes #140

---
+++ src/main/java/com/discordbolt/boltbot/discord/modules/administration/AdministrationModule.java
@@ -0,0 +1,14 @@
+package com.discordbolt.boltbot.discord.modules.administration;
+
+import com.discordbolt.boltbot.discord.api.BotModule;
+import com.discordbolt.boltbot.discord.api.CommandBean;
+import com.discordbolt.boltbot.discord.util.BeanUtil;
+import discord4j.core.DiscordClient;
+
+public class AdministrationModule implements BotModule {
+
+    @Override
+    public void initialize(DiscordClient client) {
+        BeanUtil.getBean(CommandBean.class).registerCommand(new DisconnectCommand(client));
+    }
+}
